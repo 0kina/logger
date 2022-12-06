7e4c2599d68a713b57fee23d1a32fa0c30106b85
0kina <itokon20@gmail.com>
2022-12-06 10:11:01 +0900

    fix a bug and make functions inline

--- logger.hpp
+++ logger.hpp
@@ -5,25 +5,25 @@
 namespace okina_logger {
 
 template <class T>
-void print_var_inner(std::string label, T var) {
+inline void print_var_inner(std::string label, T var) {
   std::cerr << label << ": " << var << std::endl;
 }
 
-void print_log(std::string label, int var) {
+inline void print_log(std::string label, int var) {
   print_var_inner(label, var);
 }
 
-void print_log(std::string label, std::string var) {
+inline void print_log(std::string label, std::string var) {
   print_var_inner(label, var);
 }
 
 template <class T>
-void print_vars_single_container_inner(std::string label, T vars, std::string delimiter) {
+inline void print_vars_single_container_inner(std::string label, T vars, std::string delimiter) {
   std::cerr << label << ": ";
   int i = 0;
   for (typename T::const_iterator itr = begin(vars); itr != end(vars); ++itr) {
     std::cerr << *itr;
-    if (i++ < vars.size() - 1;) {
+    if (i++ < vars.size() - 1) {
       std::cerr << delimiter;
     } else {
       std::cerr << std::endl;
@@ -31,19 +31,19 @@ void print_vars_single_container_inner(std::string label, T vars, std::string de
   }
 }
 
-void print_log(std::string label, std::vector<int> vars, std::string delimiter=", ") {
+inline void print_log(std::string label, std::vector<int> vars, std::string delimiter=", ") {
   print_vars_single_container_inner(label, vars, delimiter);
 }
 
-void print_log(std::string label, std::vector<std::string> vars, std::string delimiter=", ") {
+inline void print_log(std::string label, std::vector<std::string> vars, std::string delimiter=", ") {
   print_vars_single_container_inner(label, vars, delimiter);
 }
 
-void print_log(std::string label, std::set<int> vars, std::string delimiter=", ") {
+inline void print_log(std::string label, std::set<int> vars, std::string delimiter=", ") {
   print_vars_single_container_inner(label, vars, delimiter);
 }
 
-void print_log(std::string label, std::set<std::string> vars, std::string delimiter=", ") {
+inline void print_log(std::string label, std::set<std::string> vars, std::string delimiter=", ") {
   print_vars_single_container_inner(label, vars, delimiter);
 }
 
